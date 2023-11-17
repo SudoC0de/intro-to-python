f4fadbce12914b2ac5458fc84fd42b48073a1a85
Colin <functioncf@gmail.com>
2023-11-16 22:54:31 -0500

    Lesson 3 Practice 4 Update

--- lesson_3/practice_004_string_methods.py
+++ lesson_3/practice_004_string_methods.py
@@ -19,3 +19,5 @@ letter = input("Enter the letter to count: ")
 # Expected output:
 # I love REPLACED and dogs, but REPLACED are my favorite.
 # 3
+print(sentence.replace(new_word, 'REPLACED'))
+print(sentence.count(letter))
