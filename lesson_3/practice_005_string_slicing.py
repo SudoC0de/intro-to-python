48c7d08f45f00c18bc737f184845f499123e9e96
Colin <functioncf@gmail.com>
2023-11-16 22:31:02 -0500

    Lesson 3 Practice 5 Update

--- lesson_3/practice_005_string_slicing.py
+++ lesson_3/practice_005_string_slicing.py
@@ -2,7 +2,7 @@
 
 # 1. Slice the string and print the substring "programming".
 # 2. Slice the string and print the substring "programming is fun!"
-# 3. Slice the string and print the substring “Pto rgamn sfn”
+# 3. Slice the string and print the substring “Ph rgamn sfn”
 string = "Python programming is fun!"
 print(string[7:18])
 print(string[7:])
