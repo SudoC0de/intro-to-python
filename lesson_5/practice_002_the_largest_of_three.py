68608368f6b5ae3489d0300ef8b87a0050b51482
Colin <functioncf@gmail.com>
2023-11-28 21:09:21 -0500

    Lesson 5 Practice 2

--- lesson_5/practice_002_the_largest_of_three.py
+++ lesson_5/practice_002_the_largest_of_three.py
@@ -2,3 +2,16 @@
 #
 # Example: 124, 21, 32. Result = 124.
 
+max_number = 0
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+
+if number1 > number2 and number1 > number3:
+    max_number = number1
+elif number2 > number1 and number2 > number3:
+    max_number = number2
+elif number3 > number2 and number3 > number1:
+    max_number = number3
+
+print(max_number)
