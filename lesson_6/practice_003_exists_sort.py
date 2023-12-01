d9169aa51adec4544d7bda006cdcfaf8f89ad5ac
Colin <functioncf@gmail.com>
2023-11-30 21:41:55 -0500

    Lesson 6 Practice 3

--- lesson_6/practice_003_exists_sort.py
+++ lesson_6/practice_003_exists_sort.py
@@ -5,20 +5,15 @@
 # Some people like cheese, some people don't. ONLY add cheese to your
 # list of ingredients if you really like cheese.🧀
 
-my_list = ???
+my_list = ['Bread', 'Ham', 'Cheese', 'Mayo', 'Lettuce', 'Onion', 'Banana Pepper']
 
 # Then, use an if/else statement to print a message that will tell us
 # whether you like cheese, based on its presence in the list.
-
-
-
-???
-
+if 'Cheese' in my_list:
+    print('I like Cheese!')
 
 # Then, to make it look pretty, sort the list in alphabetical order and print it out.
 # Our computer is very old and it doesn't have a lot of memory. Also, we don't care
 # about the original order of the ingredients.
-
-???
+my_list.sort()
 print(my_list)
-
