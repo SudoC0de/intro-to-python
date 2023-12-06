9332c3468e40f0b11ca6964a29609d3b51b7063e
Colin <functioncf@gmail.com>
2023-12-05 22:06:49 -0500

    Lesson 7 Practice 4

--- lesson_7/practice_004_while.py
+++ lesson_7/practice_004_while.py
@@ -2,6 +2,10 @@
 # it prints out the number of characters in the name.
 # Modify the code so it will exit if the user enters an empty string.
 
-while ???:
+while True:
    name = input("Name: ")
-   print(f'{name} has {len(name)} characters.')
+
+   if len(name) == 0:
+      break;
+   else:
+      print(f'{name} has {len(name)} characters.')
