82e4c7912ff64ad43df6a52a0e8437f8a9b5b6a1
Colin <functioncf@gmail.com>
2023-12-05 21:08:34 -0500

    Lesson 7 Practice 2

--- lesson_7/practice_002_ranges.py
+++ lesson_7/practice_002_ranges.py
@@ -1,17 +1,21 @@
 # Figure out the values required for range() to generate the expected result:
 
 # 0, 1, 2, 3, 4
-range(???)
+print(list(range(5)))
 
 # 20, 21, 22 ... 30 (all the numbers between 20 and 30, inclusive)
-range(???)
+print(list(range(20,31)))
 
 # Even numbers: 2, 4, 6, 8, 10
-range(???)
+print(list(range(2,11,2)))
 
 # Years ending in zero between 1900 and 2000 (inclusive)
-range(???)
+print(list(range(1900,2001,10)))
+
+for year in range(1900,2001):
+    if str(year).endswith("0"):
+        print(year)
 
 # Bonus!
 # 20, 19, 18, 17, 16
-range(???)
+print(list(range(20,15,-1)))
