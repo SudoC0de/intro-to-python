a7c294607de01949228245c8ddd4b892298920ee
Colin <functioncf@gmail.com>
2023-12-07 20:42:36 -0500

    Lesson 8 Practice 1

--- lesson_8/practice_001_sum_and_multiplication.py
+++ lesson_8/practice_001_sum_and_multiplication.py
@@ -5,4 +5,15 @@
 # Input: [1, 2, 3, 4]
 # Output sum: 10
 # Output multiplication: 24
+import math
 
+numbers = [1, 2, 3, 4]
+add_answer = 0
+mult_answer = 1
+
+for number in numbers:
+    add_answer += number
+    mult_answer *= number
+
+print(add_answer)
+print(mult_answer)
