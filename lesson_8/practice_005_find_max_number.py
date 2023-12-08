9d093ae1c4975e5b51507c3ae693407b4605a048
Colin <functioncf@gmail.com>
2023-12-07 21:58:52 -0500

    Lesson 8 Practice 5

--- lesson_8/practice_005_find_max_number.py
+++ lesson_8/practice_005_find_max_number.py
@@ -2,4 +2,12 @@
 # Example:
 # numbers = [1, 2, 42, 77, 99, -100]
 # Output: 99
+numbers = [1, 2, 42, 77, 99, -100]
+result = 0
 
+for number in numbers:
+    if number > result:
+        result = number
+
+print(result)
+print(sorted(numbers)[-1])
