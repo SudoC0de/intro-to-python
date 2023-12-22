e8b8048cf2cb9aaa2ab56e6cd5537ca22c022542
Colin <functioncf@gmail.com>
2023-12-21 20:56:56 -0500

    Lesson 12 Practice 1

--- lesson_12/practice_001_empty_class.py
+++ lesson_12/practice_001_empty_class.py
@@ -3,3 +3,18 @@ Create an empty class called Car, then create two instances.
 Add the following attributes for each of the instances: Make, Model, Year.
 Create print statements to display the attributes of each one of the instances.
 """
+class Car:
+    pass
+
+Sedan = Car()
+Sedan.Make = "Chevy"
+Sedan.Model = "Prizm"
+Sedan.Year = 2001
+
+SportsCar = Car()
+SportsCar.Make = "Aston Martin"
+SportsCar.Model = "DB9"
+SportsCar.Year = 2003
+
+print(f"Sedan Make: {Sedan.Make}, Model: {Sedan.Model}, Year: {Sedan.Year}")
+print(f"SportsCar Make: {SportsCar.Make}, Model: {SportsCar.Model}, Year: {SportsCar.Year}")
