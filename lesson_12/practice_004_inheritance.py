7081838105463631ab982861b56310b9fde5d0a3
Colin <functioncf@gmail.com>
2023-12-21 22:01:42 -0500

    Lesson 12 Practice 4

--- lesson_12/practice_004_inheritance.py
+++ lesson_12/practice_004_inheritance.py
@@ -7,3 +7,20 @@ out the message: "Hello <NAME>, I'm <NAME_OF_ANIMAL>"
 Create Dog and Cat classes that inherit from Animal.
 Create instances and make them greet you.
 """
+class Animal:
+    def __init__(self, animal_name):
+        self.animal_name = animal_name
+
+    def greeting(self, name):
+        print(f"Hello {name}, I'm {self.animal_name}")
+
+class Dog(Animal):
+    pass
+
+class Cat(Animal):
+    pass
+
+new_dog = Dog("Dog")
+new_cat = Cat("Cat")
+new_dog.greeting("Colin")
+new_cat.greeting("Colin")
