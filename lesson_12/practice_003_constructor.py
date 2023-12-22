eae9fb08e56dee50dd6490ddfd183c0fb9e9588f
Colin <functioncf@gmail.com>
2023-12-21 21:44:16 -0500

    Lesson 12 Practice 3

--- lesson_12/practice_003_constructor.py
+++ lesson_12/practice_003_constructor.py
@@ -11,4 +11,25 @@ of the following models:
 Add a print_document() instance method that receives a string and prints
 it out. The printout should include the brand and model of the printer.
 """
+class Printer:
+    def __init__(self, brand, model, capacity):
+        self.brand = brand
+        self.model = model
+        self.capacity = capacity
 
+    def print_document(self, print_document):
+        print(print_document)
+
+    def notify_low_ink(self):
+        print("Ink is low")
+
+    def notify_out_of_paper(self):
+        print("Printer is out of paper")
+
+hp = Printer("HP","7955e",125)
+brother = Printer("Brother","HL-L2310D",250)
+canon = Printer("Canon","G4270",100)
+
+hp.print_document(f"Printer Brand: {hp.brand}, Model: {hp.model}, Capacity: {hp.capacity}")
+brother.print_document(f"Printer Brand: {brother.brand}, Model: {brother.model}, Capacity: {brother.capacity}")
+canon.print_document(f"Printer Brand: {canon.brand}, Model: {canon.model}, Capacity: {canon.capacity}")
