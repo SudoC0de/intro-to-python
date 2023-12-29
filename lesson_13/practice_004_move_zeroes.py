2081965a592944f5d3e5b5808ab5a43d37332832
Colin <functioncf@gmail.com>
2023-12-28 22:48:14 -0500

    Lesson 13 Practice 4

--- lesson_13/practice_004_move_zeroes.py
+++ lesson_13/practice_004_move_zeroes.py
@@ -4,4 +4,21 @@
 # Output: 4 3 12 0 0
 
 def move_zeros(arr):
-# Your code here
+    # Your code here
+    count = 0
+
+    for num in arr:
+        count += 1
+
+        if count == len(arr):
+            break
+
+        if num == 0:
+            arr.remove(0)
+            arr.append(0)
+
+    return arr
+
+custom_array = [4,0,3,0,12]
+print(f"Array: {custom_array}")
+print(f"Move Zeros Array: {move_zeros(custom_array)}")
